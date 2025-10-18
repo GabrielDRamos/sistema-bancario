4ea738158c8a50ccb2c6de0f3bfcdea35b889d6c
GabrielDRamos <gduarter21@gmail.com>
2025-10-18 11:08:38 -0300

    adicionando navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shared/components/navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('sistema_bancario');
+  protected readonly title = signal('system-bank');
 }
