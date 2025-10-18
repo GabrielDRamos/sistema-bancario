cac04fc12da73ea9010d838c046fffff51233b4d
GabrielDRamos <gduarter21@gmail.com>
2025-10-18 09:58:26 -0300

    corrigindo componentes de transacao

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroCliente } from './pages/cliente/cadastro-cliente/cadastro-cliente';
 import { ListagemCliente } from './pages/cliente/listagem-cliente/listagem-cliente';
-import { LoginForm } from './pages/auth/login-form/login-form';
+//import { LoginForm } from './pages/auth/login-form/login-form';
 import { LoginTemplate } from './pages/auth/login-template/login-template';
 import { ListagemConta } from './pages/conta/listagem-conta/listagem-conta';
 import { CadastroConta } from './pages/conta/cadastro-conta/cadastro-conta';
-import { Deposito } from './pages/operacoes/deposito/deposito';
-import { Transferencia } from './pages/operacoes/transferencia/transferencia';
-import { Saque } from './pages/operacoes/saque/saque';
+
 
 
 export const routes: Routes = [
@@ -47,23 +45,6 @@ export const routes: Routes = [
       {
         path: '',
         component: ListagemConta,
-      },
-    ]
-  },
-  {
-    path: 'operacoes',
-    children: [
-      {
-        path: 'deposito',
-        component: Deposito
-      },
-      {
-        path: 'transferencia',
-        component: Transferencia
-      },
-      {
-        path: 'saque',
-        component: Saque,
       },
     ]
   },
